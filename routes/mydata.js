9589d5197127ec4daf6ac94fcb1deb49407f9414
vyshnavi1996 <vyshu.tadipaneni14@gmail.com>
2021-03-24 19:59:59 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'vyshnavi tadipaneni' });
+  res.render('mydata', { title: 'vyshnavi tadipaneni' });
 });
 
 module.exports = router;
