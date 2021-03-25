c7b8ef740ba0025343ea102c58987125f6e83cb2
vyshnavi1996 <vyshu.tadipaneni14@gmail.com>
2021-03-24 20:34:23 -0500

    modified file

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,11 @@
-var express = require('express');
-varexpress = require('express');
-varrouter = express.Router();
-varnumber=0
+var express = require('express')
+var router = express.Router();
+var number=0
 /* GET users listing. */
 router.get('/', function(req, res, next) {​​​​​​​​
 number+=1;
 res.send(`User accesses are: ${​​​​​​​​number}​​​​​​​​`);
 }​​​​​​​​);
- 
 module.exports = router;
 
 
