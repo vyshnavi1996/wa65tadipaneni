7171eb3152cdb193028838ba28212785d1bc67a5
vyshnavi1996 <vyshu.tadipaneni14@gmail.com>
2021-03-24 20:27:52 -0500

    added function

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
-var router = express.Router();
-
+varexpress = require('express');
+varrouter = express.Router();
+varnumber=0
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+router.get('/', function(req, res, next) {​​​​​​​​
+number+=1;
+res.send(`User accesses are: ${​​​​​​​​number}​​​​​​​​`);
+}​​​​​​​​);
+ 
 module.exports = router;
+
+
